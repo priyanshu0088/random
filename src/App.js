5b4a4735593fdcfc5eb6a3961821d47834119ac7
priyanshu0088 <112187158+priyanshu0088@users.noreply.github.com>
2023-03-18 00:03:43 +0530

    original

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App () {
     
       <Routes>
       
-        <Route path="/Home" element={<Home />} /> 
+        <Route path="/" element={<Home />} /> 
         <Route path="/About" element={<About />} />  
         
         <Route path="/Contact" element={<Contact />} />
